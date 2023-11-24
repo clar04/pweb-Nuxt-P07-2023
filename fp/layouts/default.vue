1e78adbbbe34733fd73eb109b46580f9101ae6fb
clara48 <123356941+clar04@users.noreply.github.com>
2023-11-24 21:14:27 +0700

    fp

---
+++ fp/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <!-- <button class="btn btn-primary">Click Me</button>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css"
+        integrity="..."
+        crossorigin="anonymous"
+      /> -->
+    </div>
+  </template>
+  
