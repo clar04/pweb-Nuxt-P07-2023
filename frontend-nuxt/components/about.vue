2ecaeee0cb28101e30bc88f683d7ed67eacc6df2
clar04 <123356941+clar04@users.noreply.github.com>
2023-12-01 23:39:36 +0700

    Update about.vue

--- frontend-nuxt/components/about.vue
+++ frontend-nuxt/components/about.vue
@@ -6,8 +6,9 @@
                     <div class="row justify-content-center">
                         <div class="col-lg-8 col-xxl-6">
                             <div class="text-center my-5">
-                                <h1 class="fw-bolder mb-3">FP PEMWEB</h1>
-                                <p class="lead fw-normal text-muted mb-4">NUXT X PAYLOAD</p>
+                                <h1 class="fw-bolder mb-3">Our Blog</h1>
+                                <p class="lead fw-normal text-muted mb-4">Blog berita yang mencakup berbagai topik, sekaligus memenuhi
+                tugas praktikum pemrograman website</p>
                             </div>
                         </div>
                     </div>
